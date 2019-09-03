77501d75bc10d84d5d499bffd864a4b25f2ab722
Alex <o.a.momotyuk@gmail.com>
2019-09-03 10:57:13 -0500

    favorite things practice - done

--- src/main.js
+++ src/main.js
@@ -6,23 +6,10 @@
 const container = document.querySelector("#thingList")
 
 document.querySelector("#saveEntry").addEventListener("click", event => {
-    /*
-        Collect the user put by selecting the input fields, one
-        at a time, and accessing the `value` property
-    */
+    // Collect the user put by selecting the input fields, one at a time, and accessing the `value` property
     const thing = document.querySelector("#thing").value
     const location = document.querySelector("#location").value
 
-    // Once you have collected all the values, update the DOM
-    // with some HTML
-
-    //             <h1>${personName}</h1>
-    //        <div>${personAddress}</div>
-
-    container.innerHTML += `
-        <section>
-            <p>I can purchase ${thing} at ${location}</p>
-        </section>
-    `
-
+    // Once you have collected all the values, update the DOM with some HTML
+    container.innerHTML += `<div><p>I can purchase ${thing} at ${location}</p></div>`;
 })
